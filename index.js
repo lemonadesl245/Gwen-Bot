f56efdef7da69ddbc4ed8408cc84fa3399e6add5
LemonCake <126799951+lemonadesl245@users.noreply.github.com>
2023-09-25 11:52:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Eris = require("eris");
+const keep_alive = require('./keep_alive.js')
+
+
+const bot = new Eris(process.env.token);
+
+bot.on("error", (err) => {
+  console.error(err); 
+});
+
+bot.connect();
